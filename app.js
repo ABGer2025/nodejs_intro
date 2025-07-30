1b8d41ca99d2260157b9ecd01a186b4c9d58f059
ABGer2025 <abgabg2024@hotmail.com>
2025-07-30 10:46:46 +0200

    Neue Dateien app.js, config.json und package.json

---
+++ app.js
@@ -0,0 +1,17 @@
+import http from "http";
+
+
+const hostname = "127.0.0.1";
+const port = 3000;
+
+
+const server = http.createServer((req, res) => {
+  console.log(`Anfrage erhalten: ${req.method} ${req.url}`);
+    
+  res.writeHead(200, {"Content-type": "text/plain"});
+  res.end("Hello Welt vom Node.js Server!");
+});
+
+server.listen(port, hostname, () =>{
+  console.log(`Server erfolgreich gestartet unter http://${hostname}:${port}/`);
+});
